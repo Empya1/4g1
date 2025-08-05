2e22b3c6f8635ac357d6c557e90c0fd93f03a5a6
Empya1 <161871238+Empya1@users.noreply.github.com>
2025-08-05 12:57:24 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/news/2025/tinubu-ban-on-pepper")
+
+def index(): 
+	return render_template("index.html")
+	
+if __name__ == "__main__": 
+	app.run()
