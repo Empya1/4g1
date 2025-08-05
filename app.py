2c4255c5fa3d23eb650707ca6eef6bfff8b2a541
Empya1 <161871238+Empya1@users.noreply.github.com>
2025-08-05 13:22:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/news/2025/tinubu-ban-on-pepper")
 
 def index(): 
 	return render_template("index.html")
